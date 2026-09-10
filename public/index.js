166554cf8bbc4f319767d37c710023fd0baa27f1
SHIMP123 <shimp45@gmail.com>
2026-09-10 21:34:38 +0800

    implement client connection

--- public/index.js
+++ public/index.js
@@ -1,7 +1,59 @@
-const ws = new WebSocket("ws://localhost:8000");
+const ws = new WebSocket("ws://localhost:5000");
 
-ws.onopen(() => {
-    console.log("Hello from client!");
+const messageInput = document.getElementById("message-input");
+const usernameInput = document.getElementById("username-input");
+const sendButton = document.getElementById("send-button");
+const joinBtn = document.getElementById("join-button");
+const messages = document.getElementById("messages");
+
+let username = "";
+
+joinBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if(username){
+        return;
+    }
+
+    username = usernameInput.value.trim();
+
+    if(!username){
+        return;
+    }
+
+    if(ws.readyState === WebSocket.OPEN){
+        ws.send(JSON.stringify({ type: "join", username: username }));
+        usernameInput.disabled = true;
+        joinBtn.disabled = true
+    }
 })
 
-ws.send("Hello from client!");
+ws.onopen = () => {
+    console.log("Connected to server!");
+}
+
+document.getElementById("message-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const message = messageInput.value.trim();
+
+    if(!message) return;
+
+    if(ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify({type: "message", username: username, message: message}));
+        messageInput.value = "";
+    }
+    
+})
+
+ws.onmessage = (event) => {
+    const message = event.data;
+    const messageEl = document.createElement("p");
+
+    messageEl.textContent = message;
+    messages.appendChild(messageEl);
+
+    messages.scrollTop = messages.scrollHeight;
+}
+
+ws.onclose = () => {
+    console.log("Client disconnected from server");
+}
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { WebSocketServer } from "ws";
 
 const app = express();
-const PORT = Number(process.env.PORT) || 8000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -12,11 +12,54 @@ app.get("/", (req, res) => {
 })
 
 const server = app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+    console.log(`ws://localhost:5000`);
 })
 
 const wss = new WebSocketServer({ server });
 
 wss.on("connection", (ws) => {
     console.log("New client connected!");
+
+    let username = "";
+
+    ws.on("message", (message) => {
+        try{
+            console.log(`Recieved message: ${message.toString()}`);
+
+            const data = JSON.parse(message.toString());
+
+            if(data.type === "join"){
+                username = data.username
+                for(const client of wss.clients){
+                    if(client.readyState === 1){
+                        client.send(`${username} has entered the chat!`)
+                    }
+                }
+            }
+
+            if(data.type === "message"){
+                if(data.username){
+                    username = data.username;
+                }
+
+                for (const client of wss.clients) {
+                    if(client.readyState === 1){
+                        client.send(`${username}: ${data.message}`);
+                    }
+                }
+            }
+        }catch(e){
+            console.error(`Failed to parse message: ${e.message}`)
+        }
+    })
+
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
+        for(const client of wss.clients){
+            if(client.readyState === 1){
+                client.send(`${username} left the chat.`)
+            }
+        }
+    })
 })
